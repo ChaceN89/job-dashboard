b455e03e3b8fb4eaa4a1edfb3423ae1d21388039
Chace Nielson <60160140+ChaceN89@users.noreply.github.com>
2024-06-06 11:51:57 -0600

    added sorting to the job list

--- frontend/src/components/JobList.vue
+++ frontend/src/components/JobList.vue
@@ -1,5 +1,12 @@
 <template>
   <div>
+    <div class="sort-controls">
+      <button @click="setSort('date')">Sort by Date</button>
+      <button @click="setSort('id')">Sort by ID</button>
+      <button @click="toggleSortOrder">Toggle Sort Order</button>
+    </div>
+    <hr v-if="index !== jobs.length - 1" class="mx-3 border-t-2 border-gray-800 py-2">
+
     <ul>
       <!-- list all the jobs and highlight the active one -->
       <li v-for="(job, index) in jobs" 
@@ -28,8 +35,8 @@ import { formatDate } from '../utils/utils.js';
 export default {
   name: 'JobList',
   setup() {
-    // Get all the jobs (computed to recognize that it will change)
-    const jobs = computed(() => store.state.jobs);
+    // Get the sorted jobs (computed to recognize that it will change)
+    const jobs = computed(() => store.getters.sortedJobs.value);
 
     // Get the selected job to highlight the active job
     const selectedJob = computed(() => store.getters.getSelectedJob.value);
@@ -39,10 +46,20 @@ export default {
       store.actions.setSelectedId(id);
     };
 
+    const setSort = (sortBy) => {
+      store.actions.setSortBy(sortBy);
+    };
+
+    const toggleSortOrder = () => {
+      store.actions.setSortOrder(store.state.sortOrder === 'asc' ? 'desc' : 'asc');
+    };
+
     return {
       jobs,
       selectedJob,
       selectJob,
+      setSort,
+      toggleSortOrder,
       formatDate
     };
   }
--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -37,18 +37,41 @@ const state = reactive({
     },
 
   ],
-  selected_id: 1
+  selected_id: 1,
+  sortBy: 'date', // Default sorting criteria
+  sortOrder: 'asc' // Default sorting order (ascending)
 });
 
 const getters = {
   getSelectedJob: computed(() => {
     return state.jobs.find(job => job.id === state.selected_id);
+  }),
+  sortedJobs: computed(() => {
+    const jobs = [...state.jobs];
+    if (state.sortBy === 'date') {
+      jobs.sort((a, b) => {
+        const dateA = new Date(a.appointmentDate);
+        const dateB = new Date(b.appointmentDate);
+        return state.sortOrder === 'asc' ? dateA - dateB : dateB - dateA;
+      });
+    } else if (state.sortBy === 'id') {
+      jobs.sort((a, b) => {
+        return state.sortOrder === 'asc' ? a.id - b.id : b.id - a.id;
+      });
+    }
+    return jobs;
   })
 };
 
 const actions = {
   setSelectedId(newId) {
     state.selected_id = newId;
+  },
+  setSortBy(sortBy) {
+    state.sortBy = sortBy;
+  },
+  setSortOrder(sortOrder) {
+    state.sortOrder = sortOrder;
   }
 };
 
